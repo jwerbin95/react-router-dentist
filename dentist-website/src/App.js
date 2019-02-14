23aa53295470f08c304248e9158732e87d8b3454
Jacob Werbin <jwerbin95@gmail.com>
2019-02-14 13:30:52 -0600

    Completed Part Two

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Home from './Home'
-import Contacts from './Contacts'
-import Procedures from './Procedures'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
+
+import Home from './Home'
+import Contact from './Contact'
+import Procedures from './Procedures'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-        <Contacts />
-        <Procedures />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/procedures">Procedures</Link>{' '}
+            <Link to="/contact">Contact</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
