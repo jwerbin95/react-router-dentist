800330e1f4622902d5f16b8824b0ea496cf43f96
Jacob Werbin <jwerbin95@gmail.com>
2019-02-14 13:20:23 -0600

    Completed Part One

---
+++ dentist-website/src/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Contacts extends Component {
+  render() {
+    return (
+      <div>
+      	<h1>Contacts</h1>
+      	<p>These are the contacts.</p>
+      </div>
+    );
+  }
+}
---
+++ dentist-website/src/Procedures.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Procedures extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Procedures</h1>
+        <p>These are the procedures.</p>
+      </div>
+    );
+  }
+}
